255c6f4f02f5fd6cdba2dcd4cf2711f16d825f89
Konrad Dulemba <konrad@dulemba.me>
2019-04-06 22:53:26 +0200

    Update .gitignore

--- platforms/android/app/build/intermediates/assets/debug/www/js/scripts.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/scripts.js
@@ -1,62 +1,64 @@
 
-// let request = new XMLHttpRequest();
-// // request.open('GET', 'https://rhubarb-cobbler-84890.herokuapp.com/persons', true);
-// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-// request.onload = function() {
-//     // Begin accessing JSON data here
-//     let data = JSON.parse(this.response);
-//
-//     if (request.status >= 200 && request.status < 400) {
-//         data.forEach(person => {
-//             console.log(person.title)
-//         })
-//     } else {
-//         console.log('error')
-//     }
-// };
-//
-// request.send();
-
-const app = document.getElementById('root')
-
-const logo = document.createElement('img')
-logo.src = 'logo.png'
-
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
-
-app.appendChild(logo)
-app.appendChild(container)
-
-var request = new XMLHttpRequest()
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+let request = new XMLHttpRequest();
+// request.open('GET', 'https://rhubarb-cobbler-84890.herokuapp.com/persons', true);
+request.open('GET', 'https://rhubarb-cobbler-84890.herokuapp.com/greeting', true);
 request.onload = function() {
     // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
+    let data = JSON.parse(this.response);
+
+    console.log(request);
+
     if (request.status >= 200 && request.status < 400) {
-        data.forEach(movie => {
-            const card = document.createElement('div')
-            card.setAttribute('class', 'card')
-
-            const h1 = document.createElement('h1')
-            h1.textContent = movie.title
-
-            const p = document.createElement('p')
-            movie.description = movie.description.substring(0, 300)
-            p.textContent = `${movie.description}...`
-
-            container.appendChild(card)
-            card.appendChild(h1)
-            card.appendChild(p)
+        data.forEach(person => {
+            console.log(person)
         })
     } else {
-        const errorMessage = document.createElement('marquee')
-        errorMessage.textContent = `Gah, it's not working!`
-        app.appendChild(errorMessage)
+        console.log('error')
     }
-}
+};
 
-request.send()
+request.send();
+
+// const app = document.getElementById('root')
+//
+// const logo = document.createElement('img')
+// logo.src = 'logo.png'
+//
+// const container = document.createElement('div')
+// container.setAttribute('class', 'container')
+//
+// app.appendChild(logo)
+// app.appendChild(container)
+//
+// var request = new XMLHttpRequest()
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+// request.onload = function() {
+//     // Begin accessing JSON data here
+//     var data = JSON.parse(this.response)
+//     if (request.status >= 200 && request.status < 400) {
+//         data.forEach(movie => {
+//             const card = document.createElement('div')
+//             card.setAttribute('class', 'card')
+//
+//             const h1 = document.createElement('h1')
+//             h1.textContent = movie.title
+//
+//             const p = document.createElement('p')
+//             movie.description = movie.description.substring(0, 300)
+//             p.textContent = `${movie.description}...`
+//
+//             container.appendChild(card)
+//             card.appendChild(h1)
+//             card.appendChild(p)
+//         })
+//     } else {
+//         const errorMessage = document.createElement('marquee')
+//         errorMessage.textContent = `Gah, it's not working!`
+//         app.appendChild(errorMessage)
+//     }
+// }
+//
+// request.send()
 
 
 
