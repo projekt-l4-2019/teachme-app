6bba5859ae2e99d6701ed41afcb94e5ef360f0d5
Konrad Dulemba <konrad@dulemba.me>
2019-05-01 17:57:41 +0200

    Enable swipe to show sidebar, and other minor fixes

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -11,33 +11,45 @@ function onDeviceReady(){
             theme: "minimal"
         });
 
-        $('#dismiss, .overlay').on('click', function () {
-            // hide sidebar
-            $('#sidebar').removeClass('active');
-            // hide overlay
-            $('.overlay').removeClass('active');
-        });
+        $('#dismiss, .overlay').on('click', hideSidebar);
+        $('.overlay').on('swipeleft', hideSidebar);
 
-        $('#sidebarCollapse').on('click', function () {
+        $('#sidebarCollapse').on('click', showSidebar); 
+        $('html').on('swiperight', showSidebar);
+        
+        function showSidebar() {
             // open sidebar
             $('#sidebar').addClass('active');
             // fade in the overlay
             $('.overlay').addClass('active');
             $('.collapse.in').toggleClass('in');
             $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-        });
+        };
+     
+        function hideSidebar() {
+            // hide sidebar
+            $('#sidebar').removeClass('active');
+            // hide overlay
+            $('.overlay').removeClass('active');
+            
+        };
     });
 }
 
 PullToRefresh.init({
     mainElement: '.mainWindow',
+
+    shouldPullToRefresh: function(){
+        if(!document.getElementsByClassName('overlay active')[0]) return true;
+        else return false;
+    },
+
     onRefresh: function(){
-        // What do you want to do when the user does the pull-to-refresh gesture
         window.location.reload();
     },
     distThreshold : 60, // Minimum distance required to trigger the refresh.
     iconArrow: '<i class="material-icons align-content-center">arrow_downward</i>', // The icon for both instructionsPullToRefresh and instructionsReleaseToRefresh
     instructionsPullToRefresh: "Pociągnij",
     instructionsReleaseToRefresh: "Puść aby odświeżyć",
-    instructionsRefreshing: "Odświeżam..."
+    instructionsRefreshing: "Odświeżam"
 });
