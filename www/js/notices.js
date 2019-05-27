250cda60a3f7bfbb747ee1a85177ec09d27f6db7
Konrad Dulemba <konrad@dulemba.me>
2019-05-28 01:24:14 +0200

    Initial notice extended page

--- www/js/notices.js
+++ www/js/notices.js
@@ -1,27 +1,26 @@
 const url = "https://rhubarb-cobbler-84890.herokuapp.com/noticeFulls";
 
 //Get notices list from server
-if(window.location.pathname.substr(-10) === 'index.html'){
-    const app = document.getElementById('notices');
-    let notices;
-    let request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.onload = function() {
-        // Begin accessing JSON data here
-        var data = JSON.parse(this.response);
-        notices=data._embedded.noticeFulls;
-        let html='';
+const app = document.getElementById('notices');
+let notice_list;
+let request = new XMLHttpRequest();
+request.open('GET', url, true);
+request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+    notice_list = data._embedded.noticeFulls;
+    if (window.location.pathname.substr(-10) === 'index.html') {
+        let html = '';
         if (request.status >= 200 && request.status < 400) {
-            notices.forEach(notice => {
-
-                html+='<a href="notice.html" onclick="getNoticeId('+notice.id+')" class="list-group-item list-group-item-action flex-column align-items-start">';
-                html+='<div class="d-flex w-100 justify-content-between">';
-                html+='<h5 class="mb-1">' + notice.subject + '</h5>';
+            notice_list.forEach(notice => {
+                html += '<a href="notice.html" onclick="getNoticeId(' + notice.id + ')" class="list-group-item list-group-item-action flex-column align-items-start">';
+                html += '<div class="d-flex w-100 justify-content-between">';
+                html += '<h5 class="mb-1">' + notice.subject + '</h5>';
 
                 let date = new Date(notice.timestamp);
 
                 let year = date.getFullYear();
-                let month = date.getMonth()+1;
+                let month = date.getMonth() + 1;
                 let dt = date.getDate();
                 if (dt < 10) {
                     dt = '0' + dt;
@@ -30,13 +29,13 @@ if(window.location.pathname.substr(-10) === 'index.html'){
                     month = '0' + month;
                 }
 
-                html+='<small>dodano: '  + dt + '.' + month + '.' + year + '</small>';
-                html+='</div>';
+                html += '<small>dodano: ' + dt + '.' + month + '.' + year + '</small>';
+                html += '</div>';
 
                 date = new Date(notice.meeting_date);
 
                 year = date.getFullYear();
-                month = date.getMonth()+1;
+                month = date.getMonth() + 1;
                 dt = date.getDate();
                 if (dt < 10) {
                     dt = '0' + dt;
@@ -44,37 +43,45 @@ if(window.location.pathname.substr(-10) === 'index.html'){
                 if (month < 10) {
                     month = '0' + month;
                 }
-                
-                if(notice.note.length > 250){
-                    html+='<p class="mb-1">' + notice.note.substring(0,250) + "..." + '</p>';
+
+                if (notice.note.length > 250) {
+                    html += '<p class="mb-1">' + notice.note.substring(0, 250) + "..." + '</p>';
+                } else {
+                    html += '<p class="mb-1">' + notice.note + '</p>';
                 }
-                else{
-                    html+='<p class="mb-1">' + notice.note + '</p>';
-                }
-                html+='<div class="d-flex w-100 justify-content-between">';
-                html+='<h6>Data spotkania: ' + dt + '.' + month + '.' + year + '</h6>' + '<h6>' + notice.meeting_place + '</h6>' + '<small>#' + notice.id + '</small>';
-                html+='</div>';
-                html+='</a>';
+                html += '<div class="d-flex w-100 justify-content-between">';
+                html += '<h6>Data spotkania: ' + dt + '.' + month + '.' + year + '</h6>' + '<h6>' + notice.meeting_place + '</h6>' + '<small>#' + notice.id + '</small>';
+                html += '</div>';
+                html += '</a>';
             });
 
         } else {
             console.log('error');
         }
-        app.innerHTML=html;
-    };
-    request.send();
-}
+        app.innerHTML = html;
+    }
 
-function getNoticeId(id_notice){
-    var noticeID = id_notice;
-    localStorage.setItem('noticeID',noticeID);
-}
-
-if(window.location.pathname.substr(-11) === 'notice.html'){
-    let id_notice = localStorage.getItem("noticeID");
-        console.log(id_notice);
+    if (window.location.pathname.substr(-11) === 'notice.html') {
+        let id_notice = localStorage.getItem("noticeID");
+        for(var i=0; i<notice_list.length; i++)
+        {
+            if(id_notice==notice_list[i].id) break;
+        }
+        let notice=notice_list[i];
         const notice_extended = document.getElementById('notice_extended');
-        let html='';
-        html+=id_notice;
-        notice_extended.innerHTML=html;
+        let html = '';
+        html += '<h4 class="card-header">'+ notice.subject +'</h4>';
+        html += '<div class="card-body"> <p class="card-text">'+ notice.note+'</p></div>';
+        html += '<ul class="list-group list-group-flush">';
+        html += '<li class="list-group-item">Miejsce spotkania: ' + notice.meeting_place + '</li>';
+        html += '<li class="list-group-item">Cena za godzinę: ' + notice.price + ' zł </li>';
+        html += '<li class="list-group-item">Godzina: ' + notice.time_from+ ' - ' + notice.time_to + '</li>';
+        notice_extended.innerHTML = html;
+    }
+};
+request.send();
+
+function getNoticeId(id_notice) {
+    var noticeID = id_notice;
+    localStorage.setItem('noticeID', noticeID);
 }
